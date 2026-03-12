d6a2159e7dcd12b275c9b95f2e6417973425a3ed
YuvanDhurkesh <yuvansj2005@gmail.com>
2026-03-13 01:07:04 +0530

    fssai addition

--- frontend/lib/config/api_config.dart
+++ frontend/lib/config/api_config.dart
@@ -11,8 +11,8 @@ class ApiConfig {
       }
 
       // Default fallback for local development
-      return "http://localhost:5000/api";
-      // return "http://aharabackend-env.eba-nn8ggm5m.ap-south-1.elasticbeanstalk.com/api";
+      // return "http://localhost:5000/api";
+      return "http://aharabackend-env.eba-nn8ggm5m.ap-south-1.elasticbeanstalk.com/api";
     }
 
     return envUrl;
